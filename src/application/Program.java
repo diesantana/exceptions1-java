340357806a920c61898a15e48a33783afd5e01d6
Diego Santana <3c.santana@gmail.com>
2024-01-02 00:09:42 -0300

    bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,54 +9,49 @@ public class Program {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		LocalDate now = LocalDate.now();
-		
+
 		System.out.print("Room number: ");
 		int number = scanner.nextInt();
-		
+
 		System.out.print("Check-in date (dd/MM/yyyy): ");
 		String checkIn = scanner.next();
 		LocalDate checkInDate = LocalDate.parse(checkIn, Reservation.getDateFormat());
-		
+
 		System.out.print("Check-out date (dd/MM/yyyy): ");
 		String checkOut = scanner.next();
 		LocalDate checkOutDate = LocalDate.parse(checkOut, Reservation.getDateFormat());
-		
-		if(checkInDate.isBefore(now) || checkOutDate.isBefore(now)) {
-			System.out.println("Error in reservation: Reservation dates must be future dates");
-		} else if(checkOutDate.isBefore(checkInDate)) {
+
+		if (checkOutDate.isBefore(checkInDate)) {
 			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		}else {
+		} else {
 			Reservation reserve = new Reservation(number, checkInDate, checkOutDate);
-			
+
 			System.out.println(reserve);
 			System.out.println();
-			
+
 			System.out.println("Enter date to update the reservation:");
-			
+
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = scanner.next();
 			checkInDate = LocalDate.parse(checkIn, Reservation.getDateFormat());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = scanner.next();
 			checkOutDate = LocalDate.parse(checkOut, Reservation.getDateFormat());
-			
-			if(checkInDate.isBefore(now) || checkOutDate.isBefore(now)) {
-				System.out.println("Error in reservation: Reservation dates must be future dates");
-			} else if(checkOutDate.isBefore(checkInDate)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}else {
-				reserve.updateDates(
-						LocalDate.parse(checkIn, Reservation.getDateFormat()), 
-						LocalDate.parse(checkOut, Reservation.getDateFormat())
-						);
+
+			if (reserve.updateDates(checkInDate, checkOutDate) == null) {
 				
-				System.out.println(reserve);				
+				reserve.updateDates(LocalDate.parse(checkIn, Reservation.getDateFormat()),
+						LocalDate.parse(checkOut, Reservation.getDateFormat())
+				);
+
+				System.out.println(reserve);
+
+			} else {
+				System.out.println(reserve.updateDates(checkInDate, checkOutDate));
 			}
-			
-			
+
 		}
-		
+
 		scanner.close();
 	}
 }
